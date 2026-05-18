f54f48c3d2e66c8730cfa803f4fd10bb45291e9a
uventura <ualivent@gmail.com>
2026-05-18 20:36:59 -0300

    Fixing build script

--- scripts/build_thesis.sh
+++ scripts/build_thesis.sh
@@ -1,15 +1,4 @@
-#!/usr/bin/env sh
-
-set -eu
-
-SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-REPO_ROOT=$(CDPATH= cd -- "$SCRIPT_DIR/.." && pwd)
-THESIS_DIR="$REPO_ROOT/doc/thesis"
-
-cd "$THESIS_DIR"
-
 pdflatex monografia.tex
 bibtex monografia
 makeglossaries monografia
-pdflatex monografia.tex
 pdflatex monografia.tex
